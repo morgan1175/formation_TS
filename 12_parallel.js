6190ec819fd4f4ea17d88835256f11b0f652a503
morgan1175 <m.bleunven@orange.fr>
2026-01-19 14:53:22 +0100

    chore: exo11 et 12

--- 12_parallel.js
+++ 12_parallel.js
@@ -1,22 +1,33 @@
 /**
  * Utilisez la fonction Promise.all pour exécuter 3 appels API en parallèle
- * 
+ *
  * urls:
  * - https://jsonplaceholder.typicode.com/todos/1
  * - https://jsonplaceholder.typicode.com/todos/2
  * - https://jsonplaceholder.typicode.com/todos/3
- * 
+ *
  * Retournez un tableau contenant les 3 résultats
- * 
+ *
  * Vous pouvez utiliser votre fonction apiResponse créé précedement: const {apiResponse} = require("./11_async");
- * 
+ *
  * documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-*/
+ */
 
-//const {apiResponse} = require("./11_async");
+const {apiResponse} = require("./11_async");
+
 
 const parallel = () => {
+const urls = [
+    'https://jsonplaceholder.typicode.com/todos/1',
+    'https://jsonplaceholder.typicode.com/todos/2',
+    'https://jsonplaceholder.typicode.com/todos/3'
+]
 
+    return Promise.all(urls.map(() => url => apiResponse(url)))
 }
+
+parallel().then(results => {
+    console.log(results)
+})
 
 module.exports = {parallel};
