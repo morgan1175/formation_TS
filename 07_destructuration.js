aa7b2fa0b91c6c737357881bbc3840d167b8dfdc
morgan1175 <m.bleunven@orange.fr>
2026-01-19 11:50:03 +0100

    chore: exo7

--- 07_destructuration.js
+++ 07_destructuration.js
@@ -1,53 +1,60 @@
-
 /**
  * utilisez la décomposition pour extraire la premiere et la 2e case du tableau
  * retournez les dans un nouveau tableau
- * 
+ *
  * exemple: [1, 2, 3] => [1, 2]
- * 
+ *
  * astuce: vous pouvez utiliser la décomposition directement dans les arguments de la fonction
- * 
- * contrainte: 
+ *
+ * contrainte:
  *  - interdiction d'utiliser [0] et [1]
  *  - interdiction d'utiliser slice ou splice
  */
+const tab1 = [1, 2, 3, 4, 5, 6]
+const obj = {name: 'toto'}
+const obj2 = {name: "toto", password: "1234"}
 
-const extractFirstTwo = null;
+
+const extractFirstTwo = ([a, b, ...end]) => [a, b];
+console.log(extractFirstTwo(tab1))
 
 /**
  * utilisez la décomposition pour extraire la premiere case du tableau et le reste du tableau
  * retournez uniquement le reste du tableau
- * 
+ *
  * exemple: [1, 2, 3] => [2, 3]
  */
 
-const extractRest = null;
+const extractRest = ([a, ...end]) => [...end];
+console.log(extractRest(tab1))
 
 /**
  * utilisez la décomposition pour extraire le champ "name" de l'objet passé en paramètre
  * retournez le champ "name"
- * 
+ *
  * exemple: {name: "toto", age: 42} => "toto"
- * 
+ *
  * astuce: vous pouvez utiliser la décomposition directement dans les arguments de la fonction
- * 
+ *
  * contrainte:
  * - interdiction d'utiliser l'opérateur "." pour accéder au champ "name"
  */
 
-const extractName = null;
+const extractName = ({name: n, ...end}) => n;
+console.log(extractName(obj))
 
 /**
  * utilisez la décomposition pour retourner l'objet utilisateur sans le champ "password"
- * 
+ *
  * exemple: {name: "toto", password: "1234"}
- * 
- * contrainte: 
+ *
+ * contrainte:
  *    - interdiction d'utiliser "delete"
  *    - interdiction d'utiliser l'opérateur "." pour accéder au champ "password"
- * 
+ *
  */
 
-const removePassword = null;
+const removePassword = ({password: pwd, ...obj}) => ({...obj});
+console.log(removePassword(obj2))
 
 module.exports = {extractFirstTwo, extractRest, extractName, removePassword}
